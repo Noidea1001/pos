51fe82159812ed5284148ba4106b6465746e63a1
Error404 <khvysal@gmail.com>
2026-08-22 15:31:55 +0700

    fix data

--- inventory_system/settings.py
+++ inventory_system/settings.py
@@ -87,13 +87,24 @@ WSGI_APPLICATION = 'inventory_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True,
-    )
-}
+if 'VERCEL' in os.environ:
+    # ដំណើរការនៅលើ Vercel (ហៅប្រើប្រាស់ Database ខាងក្រៅ)
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL'),
+            conn_max_age=600,
+            ssl_require=True,
+        )
+    }
+else:
+    # ដំណើរការនៅលើកុំព្យូទ័រផ្ទាល់ខ្លួន (Local - ប្រើប្រាស់ SQLite លំនាំដើម)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
